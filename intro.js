5207d14b1a5abaadb8f9ad8a3e247f0874b2c73e
Ajay Shankar <ajayshankar65@yahoo.com>
2023-09-01 13:04:54 -0400

    created simple NodeJS server

--- intro.js
+++ intro.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World, this is a simple Node server');
+})
+
+server.listen(port, hostname, () => {
+    console.log(`The server is running on https//${hostname}:${port}/`);
+});
